ea7bf6af17fe8ebd64d0e29576c415e1196a7c09
kawamataryou <ba068082@gmail.com>
2021-05-09 16:53:49 +0900

    [from now] 2021/05/09 16:53:49
    diff --git a/client/.eslintrc.js b/client/.eslintrc.js
    new file mode 100644
    index 0000000..11a2465
    --- /dev/null
    +++ b/client/.eslintrc.js
    @@ -0,0 +1,30 @@
    +module.exports = {
    +    "env": {
    +        "browser": true,
    +        "es2021": true
    +    },
    +    "extends": [
    +        "eslint:recommended",
    +        "plugin:@typescript-eslint/recommended"
    +    ],
    +    "parser": "@typescript-eslint/parser",
    +    "parserOptions": {
    +        "ecmaVersion": 12,
    +        "sourceType": "module"
    +    },
    +    "plugins": [
    +        "svelte3",
    +        "@typescript-eslint"
    +    ],
    +    "overrides": [
    +      {
    +        "files": ['**/*.svelte'],
    +        "processor": 'svelte3/svelte3'
    +      }
    +    ],
    +    "settings": {
    +      'svelte3/typescript': require('typescript')
    +    },
    +    "rules": {
    +    }
    +};

---
+++ client/.eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "svelte3",
+        "@typescript-eslint"
+    ],
+    "overrides": [
+      {
+        "files": ['**/*.svelte'],
+        "processor": 'svelte3/svelte3'
+      }
+    ],
+    "settings": {
+      'svelte3/typescript': require('typescript')
+    },
+    "rules": {
+    }
+};
